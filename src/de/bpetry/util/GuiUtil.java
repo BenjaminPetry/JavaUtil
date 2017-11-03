90d8f054ae15f0bc7294a24b54ad20ee463c15e3
Benjamin Petry <benjamin.petry@synaptics.com>
2017-11-03 11:57:00 +0100

    Created a new class to make GUI related setups easier.

---
+++ src/de/bpetry/util/GuiUtil.java
@@ -0,0 +1,38 @@
+/*
+ * Benjamin Petry (www.bpetry.de)
+ * Copyright 2017 by Benjamin Petry.
+ * This software is provided on an "AS IS" BASIS,
+ * without warranties or conditions of any kind, either express or implied.
+ */
+package de.bpetry.util;
+
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TextField;
+
+/**
+ * Utility functions for GUI related issues
+ *
+ * @author Benjamin Petry
+ */
+public class GuiUtil
+{
+
+    /**
+     * Makes a TextField numeric From:
+     * https://stackoverflow.com/questions/7555564/what-is-the-recommended-way-to-make-a-numeric-textfield-in-javafx
+     *
+     * @param field the text field
+     */
+    public static void makeTextFieldNumeric(TextField field)
+    {
+        // force the field to be numeric only
+        field.textProperty().addListener(
+                (ObservableValue<? extends String> observable, String oldValue, String newValue) ->
+        {
+            if (!newValue.matches("\\d*"))
+            {
+                field.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+    }
+}
